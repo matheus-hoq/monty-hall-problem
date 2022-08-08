fdbf5049e6c228c0d76dab350d2e02565e0adf81
matheus_hoq <henriquematheus4000@gmail.com>
2022-08-08 10:02:16 -0300

    knob style

--- components/Door.jsx
+++ components/Door.jsx
@@ -6,6 +6,7 @@ export default function Porta (props) {
             <div className={styles.frame}>
                 <div className={styles.door}>
                     <div className={styles.number}>3</div>
+                    <div className={styles.knob}></div>
                 </div>
             </div>
             <div className={styles.floor}></div>
