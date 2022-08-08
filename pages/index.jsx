74434595082c05f2be1355877fabdc82284868b8
matheus_hoq <henriquematheus4000@gmail.com>
2022-08-08 13:24:51 -0300

    input added

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,10 +5,12 @@ import DoorModel from "../model/door"
 
 export default function Home() {
   const [doorOne, setDoorOne] = useState(new DoorModel(1))
+  const [text, setText] = useState("...")
 
   return (
-    <div style={{display: "flex"}}>
-      <Door door={doorOne} />
+    <div style={{display: "flex", flexDirection: "column"}}>
+      <input type="text" value={text} onChange={e => setText(e.target.value)}/>
+      <Door value={doorOne}/>
     </div>
   )
 }
