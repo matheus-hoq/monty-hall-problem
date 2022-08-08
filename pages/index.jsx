8cd1b82b9929b85bf4f453068ff769428b3c6c14
matheus_hoq <henriquematheus4000@gmail.com>
2022-08-08 09:12:29 -0300

    door component and style

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,10 @@
 import Gift from "../components/Gift"
+import Door from "../components/Door"
 
 export default function Home() {
   return (
     <div>
-      <Gift />
+      <Door />
     </div>
   )
 }
