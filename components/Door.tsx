6ccdaa92a2083db6bd49c5ad03cc99ad86754ce8
matheus_hoq <henriquematheus4000@gmail.com>
2022-08-09 09:07:45 -0300

    door opened event

--- components/Door.tsx
+++ components/Door.tsx
@@ -8,19 +8,31 @@ interface DoorProps {
 
 export default function Porta (props: DoorProps) {
     const door = props.value
-    const selected = door.selected ? styles.selected : ''
+    const selected = door.selected && !door.open ? styles.selected : ''
 
     const toggleSelection = e => props.onChange(door.toggleSelection())
+    const opened = e => {
+        e.stopPropagation()
+        props.onChange(door.opened())
+    }
+
+    function renderDoor() {
+        return (
+                <div className={styles.door}>
+                    <div className={styles.number}>{door.number}</div>
+                    <div className={styles.knob}
+                    onClick={opened}></div>
+                </div>
+            
+        )
+    }
 
     return (
         <div className={styles.area} onClick={toggleSelection}>
             <div className={`${styles.frame} ${selected}`}>
-                <div className={styles.door}>
-                    <div className={styles.number}>{door.number}</div>
-                    <div className={styles.knob}></div>
-                </div>
-            </div>
-            <div className={styles.floor}></div>
-        </div>        
+                {door.open ? false : renderDoor()}     
+            </div>          
+            <div className={styles.floor}></div>               
+        </div>  
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,9 @@ import DoorModel from "../model/door"
 
 export default function Home() {
   const [doorOne, setDoorOne] = useState(new DoorModel(1))
-  const [text, setText] = useState("...")
 
   return (
     <div style={{display: "flex", flexDirection: "column"}}>
-      <input type="text" value={text} onChange={e => setText(e.target.value)}/>
       <Door value={doorOne} onChange={newDoor => setDoorOne(newDoor)} />
     </div>
   )
